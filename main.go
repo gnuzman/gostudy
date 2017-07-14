6b62463f66bc5a02e025d35ff28f168a3e01334d
gnuzman <94644378@qq.com>
2017-07-14 14:15:00 +0800

    structs and point

--- main.go
+++ main.go
@@ -18,8 +18,26 @@ func test3(p1 string, p2 int)(int, string) {
 	return p2, p1
 }
 
+// 结构体
+type Books struct {
+	title string
+	author string
+	subject string
+	book_id int
+}
+
+// 结构体作为函数参数和返回值
+func testStruct(book *Books) *Books {
+	book.author = "zzh"
+	book.subject = "you"
+	book.title = "nice"
+	book.book_id = 1
+
+	return book
+}
+
 func main() {
-	fmt.Println("gggfff")
+	fmt.Println("ggg")
 	var a int = 10	// 声明定义为int
 	var b = 11		// 声明自动判定变量数据类型
 	c := 12			// 未声明，自动判定变量数据类型
@@ -44,5 +62,12 @@ func main() {
 	fmt.Println(test1())
 	fmt.Println(test2("1", "2"))
 	fmt.Println(test3("10", 11))
+
+
+	// 函数指针作为参数和返回值
+	var myBook Books
+	var myPtr *Books = testStruct(&myBook)
+	fmt.Println(myBook.book_id, myBook.author, myBook.title, myBook.subject)
+	fmt.Println(myPtr.book_id, myPtr.author, myPtr.title, myPtr.subject)
 
 }
